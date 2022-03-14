2e4ac729728bb528b55aac3f2b84010ced4b49a2
Rodrigo Lopes <rodvlopes@gmail.com>
2022-03-14 14:22:07 -0300

    PWM version tested and ajusted

--- fanctl-pwm.c
+++ fanctl-pwm.c
@@ -1,4 +1,9 @@
 /*
+    PWM code partially inspired by:
+    https://github.com/eclipse/mraa/blob/833bac569c5fae2905fd6f6abc90c86c784b904c/src/pwm/pwm.c 
+    and
+    https://github.com/PetrozPL/rockpi4_pwm_overlay
+
     This program has been developed to run on the ROCKPI4 to control a 5V fan connected to any GPIO through a NPN transistor.
      
     to compile: gcc fanctl.c -o fanctl
@@ -38,7 +43,7 @@ int debug = 0;
 void switch_pin (int val) {
     FILE *fp;
     char str[5];
-    sprintf(str, "%d", val);
+    sprintf(str, "%d", val?0:1);
 	fp = fopen(PWM_ENABLE_FILE, "w");
     fputs(str, fp);
 	fclose(fp);
@@ -58,13 +63,13 @@ void export_pin () {
 }
 
 void set_pwm_dc (float percentage) {
-    if (percentage < 0.25) { 
-        percentage = 0.25;
+    if (percentage < 0.9) { //it was too noisy in low freq
+		percentage = 0.0;
+	}
+    else if (percentage > 0.95) {
+        percentage = 1.0;
     }
-    else if (percentage > 1) {
-        percentage = 1;
-    }
-    int duty = percentage * PERIOD;
+    int duty = (1.0-percentage) * PERIOD; //smaller duty cycle means more power
     debug && printf("duty: %d\n", duty);
     FILE *fp;
     char str[9];
@@ -103,32 +108,24 @@ int main (int argc, char *argv[]) {
     if (argc > 1) debug = 1; //any arguments enable debug mode
 
     export_pin();
-    int is_on = read_enable();
 
-    debug && printf("CPU FAN Control Started on PWM0. is_on: %d\n", is_on);
+    debug && printf("CPU FAN Control Started on PWM0.\n");
 
+	float percentage = 0.5;
 
     while(1) {
         current_cpu_temp = read_cpu_temperature();
 
-        debug && printf("Current CPU TEMP is %d\n", current_cpu_temp);
-        if (current_cpu_temp > TEMPERATURE_THRESHOLD_ON && !is_on) {
-            switch_pin(1);
-            is_on = 1;
-            debug && printf("on\n");
-        }
-        else if (current_cpu_temp < TEMPERATURE_THRESHOLD_OFF && is_on) {
-            switch_pin(0);
-            is_on = 0;
-            debug && printf("off\n");
-        }
-
-        if (is_on) {
-            int delta_temp = current_cpu_temp - TEMPERATURE_THRESHOLD_OFF;
-            float percentage = (float) delta_temp / TEMPERATURE_THRESHOLD_DELTA;
-            debug && printf("percentage: %.9f, delta_temp: %d, TEMPERATURE_THRESHOLD_DELTA: %d\n", percentage, delta_temp, TEMPERATURE_THRESHOLD_DELTA);
-            set_pwm_dc(percentage);
-        }
+        debug && printf("Current CPU TEMP is %d, percentage: %.1f\n", current_cpu_temp, percentage);
+        int delta_temp = current_cpu_temp - TEMPERATURE_THRESHOLD_OFF;
+        percentage = (float) (delta_temp+5000) / TEMPERATURE_THRESHOLD_DELTA; //5000 to force a little bit
+        debug && printf("percentage: %.9f, delta_temp: %d, TEMPERATURE_THRESHOLD_DELTA: %d\n", percentage, delta_temp, TEMPERATURE_THRESHOLD_DELTA);
+		//char in;
+		//scanf(" %c",&in);
+		//if (in == 'a') percentage += 0.1;
+		//if (in == 'x') percentage -= 0.1;
+		//printf("read: %c\n", in); 
+		set_pwm_dc(percentage); 
         usleep(1000000); //1s
         fflush(NULL);
     }
